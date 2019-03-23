f3c808b669bfdd0c871be78364128a97e29f1369
xanvranck <vranckaert.xan@gmail.com>
2019-03-23 17:57:40 +0100

    original setup

---
+++ src/main/java/be/xan/awesomequotes/AwesomeQuotesApplication.java
@@ -0,0 +1,13 @@
+package be.xan.awesomequotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeQuotesApplication.class, args);
+	}
+
+}
